a854882f0ada573cdcba16a1c6d5e1e66044b11f
Abhijit <abhijit.dpatel@gmail.com>
2018-07-31 02:23:56 +1000

    Country Filter

--- modules/app/Controllers/Home.php
+++ modules/app/Controllers/Home.php
@@ -5,6 +5,7 @@ namespace Abhijit\App\Controllers;
 use Abhijit\Library\Controller\BaseController;
 use Abhijit\Library\View\View;
 use Abhijit\App\Service\GeoTopArtist;
+use Abhijit\App\Config;
 
 
 /**
@@ -26,10 +27,17 @@ class Home extends BaseController
     public function listAction()
     {
 		$page = (isset($_REQUEST['p']) && $_REQUEST['p']>0)?$_REQUEST['p']:1;	
-        $apiService = new GeoTopArtist();
-		$topGeoArtist = $apiService->getGeoTopArtist($page);
-		$this->controllerData['artists'] = $topGeoArtist;
-        
+		$country = (isset($_REQUEST['country']) && $_REQUEST['country']!='')?$_REQUEST['country']:Config::DEFAULT_COUNTRY;	
+		if(isset($country) && $country!='')
+		{
+        	$apiService = new GeoTopArtist();
+			$topGeoArtist = $apiService->getGeoTopArtist($page,$country);
+			$this->controllerData['artists'] = $topGeoArtist;
+			$this->controllerData['country'] = $country;
+		}
+		else
+		{
+		}
 		return new View('Home/view.phtml',$this->controllerData);
     }
 	
